f7fab6efa7e30b2594454f52891c5f517e52df68
Nikola Serhel <nikolasergel19@gmail.com>
2021-04-03 16:43:04 +0300

    deleted old project

--- src/main/java/by/serhel/arraytask/parser/ArrayParser.java
+++
@@ -1,9 +0,0 @@
-package by.serhel.arraytask.parser;
-
-import by.serhel.arraytask.exception.ArrayException;
-
-import java.util.stream.Stream;
-
-public interface ArrayParser {
-    int[] parse(Stream<String> lines) throws ArrayException;
-}
--- src/main/java/by/serhel/arraytask/reader/ArrayReader.java
+++
@@ -1,9 +0,0 @@
-package by.serhel.arraytask.reader;
-
-import by.serhel.arraytask.exception.ResourceFileException;
-
-import java.util.stream.Stream;
-
-public interface ArrayReader {
-    Stream<String> readToStream(String path) throws ResourceFileException;
-}
