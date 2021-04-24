9914100ae413b9cd148e7c9b535107b989ce0a09
Nikola Serhel <nikolasergel19@gmail.com>
2021-04-24 17:30:47 +0300

    getAbsolutaPath method removed. Data moved into root of project.

--- shapes-task/src/main/java/by/serhel/shapestask/reader/CustomFileReader.java
+++ shapes-task/src/main/java/by/serhel/shapestask/reader/CustomFileReader.java
@@ -2,17 +2,15 @@ package by.serhel.shapestask.reader;
 
 import by.serhel.shapestask.exception.CustomFileException;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class CustomFileReader {
+
     public Stream<String> readLines(String filePath) throws CustomFileException {
-        filePath = getAbsolutePath(filePath);
         Path path = Paths.get(filePath);
         Stream<String> lines = null;
         try {
@@ -21,15 +19,6 @@ public class CustomFileReader {
             throw new CustomFileException("File reading was failed: " + path, e);
         }
         return lines;
-    }
-
-    public String getAbsolutePath(String path) throws CustomFileException {
-        ClassLoader loader = getClass().getClassLoader();
-        URL url = loader.getResource(path);
-        if(url == null){
-            throw new CustomFileException("Path is not found: " + path);
-        }
-        return url.getPath();
     }
 }
 
